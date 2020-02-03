5e24e5fdc39d9b2ff1b3aeddcfd090a91f97a8e9
Phil <pmasson993@gmail.com>
2020-02-03 16:35:22 -0500

    Added background graphs

--- MATLAB/BasicGraph.m
+++ MATLAB/BasicGraph.m
@@ -3,18 +3,18 @@
 
 %import sample data
 %data is in following format: Channel, Energy, Count
-csvDataSample = csvread('SAMPX_CO.CSV'); %change this for different trials
+csvDataSample = csvread('BK.CSV'); %change this for different trials
 
 %plot data (Energy vs Count)
 fig1 = figure('Position', [10 10 900 400]);
 plot(csvDataSample(:,2), csvDataSample(:,3));
 xlabel('Energy (keV)');
 ylabel('Counts');
-title('Graph of Sample X and Source Co57'); %------------
+title('Background Graph Without Any Sources'); %------------
 xlim([0,140]); %set limit on x-axis
-filename = 'Sample_X'; %-----------------
+filename = 'BKG_NoSource'; %-----------------
 saveas(fig1,filename,'png');
-ylim([0,5e4]); %set limit on y-axis
-title('Graph of Sample X and Source Co57 (Zoomed)'); %--------------
+ylim([0,2e4]); %set limit on y-axis
+title('Background Graph Without Any Sources (Zoomed)'); %--------------
 saveas(fig1,[filename, '_ZOOM'],'png');
 
